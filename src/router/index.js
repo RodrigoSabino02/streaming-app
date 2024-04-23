392926d5e427e0a8b0e00ffaa54073a4f2fd0216
Your Name <rodrigosabinodev@gmail.com>
2024-04-23 14:49:39 -0300

    routes

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: () => import('../pages/Movies.vue')
+    },
+    {
+        path: '/series',
+        name: 'series',
+        component: () => import('../pages/Series.vue')
+      },
+      {
+        path: '/my-list',
+        name: 'my list',
+        component: () => import('../pages/MyList.vue')
+      },
+      {
+        path: '/details',
+        name: 'details',
+        component: () => import('../pages/Details.vue')
+      }
+  ]
+})
+
+export default router
