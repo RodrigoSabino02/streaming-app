e86b616c3208bf954e13eee337fb20be85d97f71
Your Name <rodrigosabinodev@gmail.com>
2024-04-23 14:48:51 -0300

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: "jit",
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
